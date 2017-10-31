3c0714b411f3141a30c3216739e48a8c061cc6e4
JCRaymond <raymond.jacob17@gmail.com>
2017-10-31 16:31:22 -0400

    Parts 1-3 complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
 
+require 'byebug'
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +21,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  i1 = page.body.index(e1)
+  i2 = page.body.index(e2)
+  expect(i1).to be < i2
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +34,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(", ").each do |rating|
+    if uncheck.nil?
+      step(%Q{I check "ratings[#{rating}]"})
+    else
+      step(%Q{I uncheck "ratings[#{rating}]"})
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  match = /<tbody>(.*)<\/tbody>/m.match(page.body)
+  rows = match[1].scan(/<tr>/m).length
+  expect(rows.to_i).to be Movie.count
 end
